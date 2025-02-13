f2d77427b946dab3848cc8316e93485522e07be6
thong312 <lyminhthong312@gmail.com>
2025-02-13 11:06:17 +0700

    2nd commit

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
+// open the database file
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,11 +34,19 @@ app.get('/', (req, res) => {
 
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg); // Broadcast the message to all clients
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        // TODO handle the failure
+        return;
+      }
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
+    });
   });
-});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
